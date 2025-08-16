44e67e5b1996acf29d7be9f8c5fb1a35ac10ef37
ChenSiYuan <2724989305@qq.com>
2025-08-17 06:06:20 +0800

    proxyRef

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -72,3 +72,23 @@ export function toRefs() {
     }
     return res;
 }
+
+export function proxyRef(objectWithRef) { //代理ref，effect中使用ref不用.value
+    return new Proxy(objectWithRef, {
+        get(target, key, receiver) {
+            let r = Reflect.get(target, key, receiver)
+            return r._v_isRef ? r.value : r; //自动脱ref
+        },
+        set(target, key, value, receiver) {
+            const oldValue = target[key];
+            if (oldValue !== value) {
+                if (oldValue._v_isRef) {
+                    oldValue.value = value; // 如果老值是ref，需要给ref赋值
+                    return true;
+                } else {
+                    return Reflect.set(target, key, value, receiver);
+                }
+            }
+        }
+    })
+}
