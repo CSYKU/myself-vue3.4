e1f447f0b334f02e1ec36db543e02babb3310400
ChenSiYuan <2724989305@qq.com>
2025-08-21 21:33:28 +0800

    runtime-core 会引入reactivity，修改引入导出和package.json

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,9 +1,9 @@
-export * from "@vue/reactivity";
-
 import { nodeOpers } from "./nodeOps";
 import patchProp from "./patchProp";
+import { createRenderer } from "@vue/runtime-core"  //runtime-core 会引入reactivity，修改引入导出和package.json
 
 // 将节点操作和属性操作合并在一起
 const rederOptions = Object.assign({ patchProp }, nodeOpers)
+export { rederOptions } //
 
-function createRenderer() { }
+export * from "@vue/runtime-core";
