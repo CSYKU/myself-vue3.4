261b86e0f2212fa98222325d944e944536c22937
ChenSiYuan <2724989305@qq.com>
2025-08-21 22:53:44 +0800

    解构重名

--- packages/runtime-core/src/index.ts
+++ packages/runtime-core/src/index.ts
@@ -1,5 +1,15 @@
 export function createRenderer(renderOptions) {
     // core中不关心如何渲染
+    const {
+        insert: hostInsert,
+        remove: hostRemove,
+        createElement: hostCreateEment,
+        setText: hostSetText,
+        setElementText: hostSetElementText,
+        parentNode: hostParentNode,
+        nextSibling: hostNextSibling,
+        patchProp: hostPatchProp,
+    } = renderOptions;
 
     const render = (vnode, container) => { };
     return {
