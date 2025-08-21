c45cba2c189b4e718b932d40ab02bc73a040a847
ChenSiYuan <2724989305@qq.com>
2025-08-22 00:15:56 +0800

    写错bug

--- packages/runtime-dom/src/moudles/patchAtter.ts
+++ packages/runtime-dom/src/moudles/patchAtter.ts
@@ -3,6 +3,6 @@ export default function patchAtter(el, key, value) {
     if (value = null) {
         el.removeAttribute(key);
     } else {
-        el.setAtteriute(key, value);
+        el.setAttribute(key, value);
     }
 }
